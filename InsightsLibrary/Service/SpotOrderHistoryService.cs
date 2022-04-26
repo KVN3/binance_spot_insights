df5c7fb298895cc7f75fc3e268bcfe3164f8d851
Kevin <kvn3@protonmail.com>
2022-04-26 14:31:34 +0200

    quick and dirty fix for ranges > 500 trades

--- InsightsLibrary/Service/SpotOrderHistoryService.cs
+++ InsightsLibrary/Service/SpotOrderHistoryService.cs
@@ -27,10 +27,9 @@ namespace InsightsLibrary.Service
 
         public async Task<TradeReportResult> GetTradeReports(string symbol)
         {
-            var userTradesResult = await binanceClient.SpotApi.Trading.GetUserTradesAsync(symbol);
-            userTradesResult.GetResultOrError(out IEnumerable<BinanceTrade> result, out Error error);
+            await GetAllUserTrades(symbol);
 
-            var groups = result.GroupBy(key =>
+            var groups = userTrades.GroupBy(key =>
                 new DateTime(year: key.Timestamp.Year, month: key.Timestamp.Month, day: key.Timestamp.Day));
 
             List<TradeReport> reports = new List<TradeReport>();
@@ -43,6 +42,71 @@ namespace InsightsLibrary.Service
             }
 
             return new TradeReportResult(reports);
+        }
+
+        private List<BinanceTrade> userTrades;
+
+        private async Task GetAllUserTrades(string symbol, DateTime? startTime = null, DateTime? endTime = null)
+        {
+            // Last 500 orders and start
+            if (startTime == null && endTime == null)
+                userTrades = new List<BinanceTrade>();
+
+            var userTradesResult = await binanceClient.SpotApi.Trading.GetUserTradesAsync(symbol, startTime: startTime, endTime: endTime);
+            userTradesResult.GetResultOrError(out IEnumerable<BinanceTrade> result, out Error error);
+
+            if (result != null && result.Count() > 0)
+            {
+                var newRange = result.ToList();
+                var firstTradeInRange = newRange[0];
+
+                // First trade found, we stop looping
+                if (userTrades.Count > 0 && firstTradeInRange.Id == userTrades[0].Id)
+                    return;
+
+                // We do this to avoid missing trades at the same time and the end of the range:
+                // Limit 500, there's 2 trades at 16:00:00, but the second trade is the 501th trade
+                // So we can't just decrease the endTime by a second, as we would miss the 2nd trade at 16:00
+                // Trash solution is to pass the same timestamp as endTime and filter double trades out at the end of the range
+                // Can still miss a trade if limit is met at the same stamp
+                var indexesToRemove = new List<int>();
+
+                for (int i = newRange.Count() - 1; i >= 0; i--)
+                {
+                    bool isDuplicate = false;
+
+                    for (int x = 0; x < userTrades.Count(); x++)
+                    {
+                        if (newRange[i].Id == userTrades[x].Id)
+                        {
+                            isDuplicate = true;
+                            indexesToRemove.Add(i);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // Need only check the rear
+                    if (!isDuplicate)
+                        break;
+                }
+
+                foreach (int i in indexesToRemove)
+                {
+                    newRange.RemoveAt(i);
+                }
+
+                userTrades.InsertRange(0, newRange);
+
+                // Next iter
+                startTime = firstTradeInRange.Timestamp.AddDays(-1);
+                endTime = firstTradeInRange.Timestamp;
+                await GetAllUserTrades(symbol, startTime, endTime);
+            }
+
+            return;
         }
     }
 }
